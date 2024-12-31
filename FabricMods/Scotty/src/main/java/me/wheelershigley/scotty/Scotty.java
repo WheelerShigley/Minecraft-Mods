917c3c7129101c506616ef92ed51e15623e08136
WheelerShigley <115506442+WheelerShigley@users.noreply.github.com>
2024-12-31 16:49:52 -0600

    Scotty!!!

---
+++ FabricMods/Scotty/src/main/java/me/wheelershigley/scotty/Scotty.java
@@ -0,0 +1,9 @@
+package me.wheelershigley.scotty;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Scotty implements ModInitializer {
+
+    @Override
+    public void onInitialize() {}
+}
