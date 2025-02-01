62dcf4d064ce9db2d17e3b7b26c5785f7a5938e2
WheelerShigley <115506442+WheelerShigley@users.noreply.github.com>
2025-02-01 15:44:45 -0600

    HydraDrop: PlayerHead Drop

---
+++ FabricMods/HydraDrop/1.21.3, 1.21.4/src/main/java/me/wheelershigley/hydra_drop/mixin/PlayerMixin.java
@@ -0,0 +1,37 @@
+package me.wheelershigley.hydra_drop.mixin;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.ItemEntity;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.mob.CreeperEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.server.network.ServerPlayerEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ServerPlayerEntity.class)
+public abstract class PlayerMixin {
+    @Shadow public abstract ItemEntity dropItem(ItemStack stack, boolean throwRandomly, boolean retainOwnership);
+
+    @Inject(
+        method = "onDeath",
+        at = @At("HEAD")
+    )
+    public void onDeath(DamageSource damageSource, CallbackInfo ci) {
+        Entity e_Killer = damageSource.getAttacker();
+        if(
+            (e_Killer instanceof CreeperEntity)
+            && ( (CreeperEntity)e_Killer ).isCharged()
+        ) {
+            this.dropItem(
+                new ItemStack(Items.PLAYER_HEAD),
+                true,
+                true
+            );
+        }
+    }
+}
