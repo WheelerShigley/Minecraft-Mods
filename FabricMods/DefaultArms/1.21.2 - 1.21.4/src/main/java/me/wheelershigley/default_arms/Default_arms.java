8baaed66f4723b0722f22c6c522306c6d1250cc8
WheelerShigley <115506442+WheelerShigley@users.noreply.github.com>
2025-02-01 19:08:58 -0600

    ArmorStandArms Images
    Created Icon and Example images

---
+++ FabricMods/DefaultArms/1.21.2 - 1.21.4/src/main/java/me/wheelershigley/default_arms/Default_arms.java
@@ -0,0 +1,8 @@
+package me.wheelershigley.default_arms;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Default_arms implements ModInitializer {
+    //public static final Logger logger = LoggerFactory.getLogger("default_arms");
+    @Override public void onInitialize() {}
+}
