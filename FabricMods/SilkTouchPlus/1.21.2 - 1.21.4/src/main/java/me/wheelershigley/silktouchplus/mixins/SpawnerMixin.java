556674641a6300a43e0bf702fbf04ce4b30842eb
WheelerShigley <115506442+WheelerShigley@users.noreply.github.com>
2025-02-23 13:52:46 -0600

    SilkTouchPlus 1.21.2+ Attempt
    Incomplete implementation of update

---
+++ FabricMods/SilkTouchPlus/1.21.2 - 1.21.4/src/main/java/me/wheelershigley/silktouchplus/mixins/SpawnerMixin.java
@@ -0,0 +1,66 @@
+package me.wheelershigley.silktouchplus.mixins;
+
+import com.mojang.serialization.MapCodec;
+import me.wheelershigley.silktouchplus.helpers.EnchantmentsHelper;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.BlockWithEntity;
+import net.minecraft.block.SpawnerBlock;
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.enchantment.Enchantments;
+import net.minecraft.item.ItemStack;
+import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.Set;
+
+@Mixin(SpawnerBlock.class)
+public abstract class SpawnerMixin extends BlockWithEntity  {
+    protected SpawnerMixin(Settings settings) {
+        super(settings);
+    }
+
+    @Final
+    @Shadow
+    public static MapCodec<SpawnerBlock> CODEC;
+
+    public MapCodec<SpawnerBlock> getCodec() {
+        return CODEC;
+    }
+
+    /**
+     * @author wheelershigley
+     * @reason Since spawners drop with SilkTouch, it should not yield Exp when dropped.
+     */
+    @Inject(
+        method = "onStacksDropped",
+        at = @At("HEAD"),
+        cancellable = true
+    )
+    protected void onStacksDropped(BlockState state, ServerWorld world, BlockPos pos, ItemStack tool, boolean dropExperience, CallbackInfo ci) {
+        /* Exp. should not drop if the spawner is silk-touched. */ {
+            Set< RegistryEntry<Enchantment> > enchants = EnchantmentHelper.getEnchantments(tool).getEnchantments();
+            if( EnchantmentsHelper.includesEnchantment(enchants, Enchantments.SILK_TOUCH) ) {
+                dropExperience = false;
+            }
+        }
+
+        /* Vanilla Implementation */ {
+            super.onStacksDropped(state, world, pos, tool, dropExperience);
+            if(dropExperience) {
+                int i = 15 + world.random.nextInt(15) + world.random.nextInt(15);
+                this.dropExperience(world, pos, i);
+            }
+        }
+
+        ci.cancel();
+    }
+}
