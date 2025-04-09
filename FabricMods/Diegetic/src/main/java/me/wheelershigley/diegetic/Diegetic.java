a47c17defa05c9f1c17a7d4c13ae7e48e10d12e4
WheelerShigley <115506442+WheelerShigley@users.noreply.github.com>
2025-04-09 15:40:52 -0500

    Diegetic Initialization

---
+++ FabricMods/Diegetic/src/main/java/me/wheelershigley/diegetic/Diegetic.java
@@ -0,0 +1,16 @@
+package me.wheelershigley.diegetic;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+public class Diegetic implements ModInitializer {
+    public static final String MOD_ID = "diegetic";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    @Override public void onInitialize() {}
+}
