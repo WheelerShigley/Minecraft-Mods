68ec56db8bbdc648e53f1727946efc711a40139d
WheelerShigley <115506442+WheelerShigley@users.noreply.github.com>
2025-04-11 14:02:22 -0500

    PodzolLitter initialization

---
+++ FabricMods/PodzolLitter/-1.21.5/src/main/java/me/wheelershigley/podzol_litter/PodzolLitter.java
@@ -0,0 +1,7 @@
+package me.wheelershigley.podzol_litter;
+
+import net.fabricmc.api.ModInitializer;
+
+public class PodzolLitter implements ModInitializer {
+    @Override public void onInitialize() {}
+}
