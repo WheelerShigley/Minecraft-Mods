7a868c7766a5d30eff5fee9e1ff8b81f589fd712
WheelerShigley <115506442+WheelerShigley@users.noreply.github.com>
2025-04-11 11:06:12 -0500

    ServerPetTransfer initialization

---
+++ FabricMods/ServerPetTransfer/-1.21.5/src/main/java/me/wheelershigley/server_pet_transfer/ServerPetTransfer.java
@@ -0,0 +1,7 @@
+package me.wheelershigley.server_pet_transfer;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ServerPetTransfer implements ModInitializer {
+    @Override public void onInitialize() {}
+}
