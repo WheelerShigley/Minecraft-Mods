d8c464ab22816f7d64b42331cba6dac9f48da413
WheelerShigley <115506442+WheelerShigley@users.noreply.github.com>
2025-04-17 13:00:33 -0500

    PortalNetwork Initialization

---
+++ FabricMods/PoralNetwork/1.21.5/src/main/java/me/wheelershigley/portal_network/PortalNetwork.java
@@ -0,0 +1,10 @@
+package me.wheelershigley.portal_network;
+
+import net.fabricmc.api.ModInitializer;
+
+public class PortalNetwork implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
