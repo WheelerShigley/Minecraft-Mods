c81903619433382a69f141ae05a5a066e53a9ed4
WheelerShigley <115506442+WheelerShigley@users.noreply.github.com>
2025-04-18 17:22:57 -0500

    Neotony Initialization

---
+++ FabricMods/Neotony/1.21.5/src/main/java/me/wheelershigley/neotony/Neotony.java
@@ -0,0 +1,13 @@
+package me.wheelershigley.neotony;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.LoggerFactory;
+
+import org.slf4j.Logger;
+
+public class Neotony implements ModInitializer {
+    public static final String MOD_ID = "neotony";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    @Override public void onInitialize() {}
+}
