94560725cff002dc8f050f87be1da83ff4fb156f
WheelerShigley <115506442+WheelerShigley@users.noreply.github.com>
2025-04-22 15:10:20 -0500

    ConfigureWanderingFrequency initialization

---
+++ FabricMods/ConfigureWanderingFrequency/1.21.5/src/main/java/me/wheelershigley/configure_wandering_frequency/ConfigureWanderingFrequency.java
@@ -0,0 +1,7 @@
+package me.wheelershigley.configure_wandering_frequency;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ConfigureWanderingFrequency implements ModInitializer {
+    @Override public void onInitialize() {}
+}
