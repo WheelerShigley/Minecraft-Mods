19dfdb6cf06059f70720bdcb85993916df481dae
Wheeler-Shigley <115506442+WheelerShigley@users.noreply.github.com>
2025-04-30 09:27:20 -0500

    LilGuy 1.21.5 Particle BugFix

---
+++ FabricMods/LilGuy/1.21.5/src/main/java/me/wheelershigley/lil_guy/mixins/EntityMixin.java
@@ -0,0 +1,51 @@
+package me.wheelershigley.lil_guy.mixins;
+
+import net.minecraft.component.DataComponentTypes;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.data.DataTracked;
+import net.minecraft.entity.data.DataTracker;
+import net.minecraft.entity.data.TrackedData;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.text.Text;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.Optional;
+
+@Mixin(Entity.class)
+public abstract class EntityMixin implements DataTracked {
+
+    @Shadow @Final private static TrackedData< Optional<Text> > CUSTOM_NAME;
+    @Shadow @Final protected DataTracker dataTracker;
+    @Shadow @Nullable public abstract Text getCustomName();
+
+    @Inject(
+        method = "interact",
+        at = @At("TAIL"),
+        cancellable = true
+    )
+    public void interact(PlayerEntity player, Hand hand, CallbackInfoReturnable<ActionResult> cir) {
+        ItemStack interactionItem = player.getStackInHand(hand);
+        if(
+            interactionItem.isOf(Items.NAME_TAG)
+            && !interactionItem.contains(DataComponentTypes.CUSTOM_NAME)
+            && !Text.empty().equals( this.getCustomName() )
+        ) {
+            //Remove custom name
+            this.dataTracker.set( CUSTOM_NAME, Optional.empty() );
+
+            //use item
+            interactionItem.decrement(1);
+            cir.setReturnValue(ActionResult.SUCCESS);
+        }
+    }
+}
