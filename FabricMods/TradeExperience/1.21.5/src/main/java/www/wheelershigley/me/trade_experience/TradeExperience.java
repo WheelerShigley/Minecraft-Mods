7a315142c6f26df2fedcd284903f072cb99b8a47
Wheeler-Shigley <115506442+WheelerShigley@users.noreply.github.com>
2025-05-02 11:12:00 -0500

    TradeExperience Initialiation

---
+++ FabricMods/TradeExperience/1.21.5/src/main/java/www/wheelershigley/me/trade_experience/TradeExperience.java
@@ -0,0 +1,12 @@
+package www.wheelershigley.me.trade_experience;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TradeExperience implements ModInitializer {
+    public static final String MOD_ID = "trade_experience";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    @Override public void onInitialize() {}
+}
