17b0b0aa38ae07b74ead9e25081f97266c460acd
Wheeler-Shigley <115506442+WheelerShigley@users.noreply.github.com>
2025-06-07 09:34:07 -0500

    LilGuy 1.20.5-1.21.5 Singleplayer Support

--- FabricMods/LilGuy/1.20.5 - 1.21.5/src/main/java/me/wheelershigley/lil_guy/mixins/PassiveEntityMixin.java
+++ FabricMods/LilGuy/1.20.5 - 1.21.5/src/main/java/me/wheelershigley/lil_guy/mixins/PassiveEntityMixin.java
@@ -1,13 +1,18 @@
 package me.wheelershigley.lil_guy.mixins;
 
+import me.wheelershigley.lil_guy.LilGuy;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.data.TrackedData;
 import net.minecraft.entity.mob.PathAwareEntity;
 import net.minecraft.entity.passive.PassiveEntity;
 import net.minecraft.particle.ParticleTypes;
 import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(PassiveEntity.class)
 public class PassiveEntityMixin extends PathAwareEntity {
@@ -15,19 +20,40 @@ public class PassiveEntityMixin extends PathAwareEntity {
         super(entityType, world);
     }
 
-    @Shadow protected int happyTicksRemaining;
     @Shadow public int getBreedingAge() { return 0; }
     @Shadow public void setBreedingAge(int age) {}
+    @Shadow protected int happyTicksRemaining;
+
+    @Shadow @Final private static TrackedData<Boolean> CHILD;
 
     /**
      * @author Wheeler-Shigley
      * @reason Named baby animals will not have their age changed.
      */
-    @Overwrite
-    public void tickMovement() {
+    @Inject(
+        method = "tickMovement",
+        at = @At("HEAD"),
+        cancellable = true
+    )
+    public void tickMovement(CallbackInfo ci) {
         super.tickMovement();
+        if(this.getWorld().isClient) {
+            if(this.happyTicksRemaining > 0) {
+                if(this.happyTicksRemaining%4 == 0) {
+                    this.getWorld().addParticleClient(
+                        ParticleTypes.HAPPY_VILLAGER,
+                        this.getParticleX( (double)1.0F ),
+                        this.getRandomBodyY() + (double)0.5F,
+                        this.getParticleZ( (double)1.0F ),
+                        (double)0.0F,
+                        (double)0.0F,
+                        (double)0.0F
+                    );
+                }
 
-        if( this.isAlive() ) {
+                --this.happyTicksRemaining;
+            }
+        } else if( this.isAlive() ) {
             int breeding_age = this.getBreedingAge();
             //Babies
             if(breeding_age < 0 && !this.hasCustomName() ) {
@@ -40,5 +66,6 @@ public class PassiveEntityMixin extends PathAwareEntity {
                 this.setBreedingAge(breeding_age);
             }
         }
+        ci.cancel();
     }
 }
