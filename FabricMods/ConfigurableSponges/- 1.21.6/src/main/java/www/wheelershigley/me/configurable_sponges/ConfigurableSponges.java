1e6d254835d63099e812af1865e2279655802552
Wheeler-Shigley <115506442+WheelerShigley@users.noreply.github.com>
2025-06-13 13:31:59 -0500

    ConfigurableSponges Initialization

---
+++ FabricMods/ConfigurableSponges/- 1.21.6/src/main/java/www/wheelershigley/me/configurable_sponges/ConfigurableSponges.java
@@ -0,0 +1,12 @@
+package www.wheelershigley.me.configurable_sponges;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ConfigurableSponges implements ModInitializer {
+    public static final String MOD_ID = "configurable_sponges";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    @Override public void onInitialize() {}
+}
---
+++ FabricMods/ConfigurableSponges/- 1.21.6/src/main/java/www/wheelershigley/me/configurable_sponges/client/ConfigurableSpongesClient.java
@@ -0,0 +1,7 @@
+package www.wheelershigley.me.configurable_sponges.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ConfigurableSpongesClient implements ClientModInitializer {
+    @Override public void onInitializeClient() {}
+}
