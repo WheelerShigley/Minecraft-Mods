25b141fedc4affff2ad7641aa13cc68c3786a779
Wheeler-Shigley <115506442+WheelerShigley@users.noreply.github.com>
2025-06-17 09:37:31 -0500

    Bleu 1.21.6 + Singleplayer Support

---
+++ FabricMods/Bleu/1.21.2 - 1.21.5/src/client/java/www/wheelershigley/me/bleu/client/BleuClient.java
@@ -0,0 +1,10 @@
+package www.wheelershigley.me.bleu.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BleuClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
