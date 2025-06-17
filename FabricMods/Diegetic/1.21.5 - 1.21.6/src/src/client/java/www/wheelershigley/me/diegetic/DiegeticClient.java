1a42c33bd06adea629b7b0fe0037608e0d812f0a
Wheeler-Shigley <115506442+WheelerShigley@users.noreply.github.com>
2025-06-17 09:56:39 -0500

    Diegetic 1.21.6 + Partial Singleplayer Support

---
+++ FabricMods/Diegetic/1.21.5 - 1.21.6/src/src/client/java/www/wheelershigley/me/diegetic/DiegeticClient.java
@@ -0,0 +1,7 @@
+package www.wheelershigley.me.diegetic.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DiegeticClient implements ClientModInitializer {
+    @Override public void onInitializeClient() {}
+}
