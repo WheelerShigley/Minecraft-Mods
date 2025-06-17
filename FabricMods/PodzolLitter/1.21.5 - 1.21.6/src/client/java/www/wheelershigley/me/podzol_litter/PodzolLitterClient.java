f5ed25698dffa467f1b502f02e165a8a60fb32c2
Wheeler-Shigley <115506442+WheelerShigley@users.noreply.github.com>
2025-06-17 10:08:22 -0500

    PodzolLitter 1.21.6 + Singleplayer Support

---
+++ FabricMods/PodzolLitter/1.21.5 - 1.21.6/src/client/java/www/wheelershigley/me/podzol_litter/PodzolLitterClient.java
@@ -0,0 +1,7 @@
+package www.wheelershigley.me.podzol_litter.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PodzolLitterClient implements ClientModInitializer {
+    @Override public void onInitializeClient() {}
+}
