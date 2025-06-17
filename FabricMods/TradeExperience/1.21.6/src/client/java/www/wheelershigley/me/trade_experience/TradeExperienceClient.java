91eb5b8b6d16e26d78a14007b0067ae3accff52e
Wheeler-Shigley <115506442+WheelerShigley@users.noreply.github.com>
2025-06-17 09:19:44 -0500

    TradeExperience 1.21.6 + Singleplayer Support

---
+++ FabricMods/TradeExperience/1.21.6/src/client/java/www/wheelershigley/me/trade_experience/TradeExperienceClient.java
@@ -0,0 +1,7 @@
+package www.wheelershigley.me.trade_experience;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TradeExperienceClient implements ClientModInitializer {
+    @Override public void onInitializeClient() {}
+}
---
+++ FabricMods/TradeExperience/1.21.6/src/main/java/www/wheelershigley/me/trade_experience/helpers/ConfigurationHelper.java
@@ -0,0 +1,44 @@
+package www.wheelershigley.me.trade_experience.helpers;
+
+import net.fabricmc.loader.api.FabricLoader;
+import www.wheelershigley.me.trade_experience.config.Configuration;
+import www.wheelershigley.me.trade_experience.config.Configurations;
+
+import java.io.File;
+
+import static www.wheelershigley.me.trade_experience.TradeExperience.MOD_ID;
+
+public class ConfigurationHelper {
+    public static Configurations createTradeExperienceConfigurations() {
+        final String configurationsFileName = (MOD_ID + ".properties").toLowerCase();
+        final File configurationsFile = FabricLoader.getInstance().getConfigDir().resolve(configurationsFileName).toFile();
+        final Configurations configurations = new Configurations(configurationsFile, configurationsFileName);
+
+        configurations.addConfiguration(
+            new Configuration<>(
+                "trade_timeout_time",
+                30L,
+                "How many seconds before a trade times out."
+            )
+        );
+        configurations.addConfiguration(
+            new Configuration<>(
+                "experience_name",
+                "experience",
+                "Monetary-like experience name."
+            )
+        );
+        configurations.addConfiguration(
+            new Configuration<>(
+                "external_balance_permission_requirement",
+                4L,
+                new String[]{
+                    "Permission level to check other's balances",
+                    "Default permission-levels are 0 for no-requirement and 4 for operator-requirement"
+                }
+            )
+        );
+
+        return configurations;
+    }
+}
