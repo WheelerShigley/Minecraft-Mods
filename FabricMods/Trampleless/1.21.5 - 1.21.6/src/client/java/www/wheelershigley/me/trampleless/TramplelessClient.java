20e5182d1bead0c2368c507ba88d7ee5079ed4c1
Wheeler-Shigley <115506442+WheelerShigley@users.noreply.github.com>
2025-06-17 12:22:46 -0500

    Trampleless Minor Changes

---
+++ FabricMods/Trampleless/1.21.5 - 1.21.6/src/client/java/www/wheelershigley/me/trampleless/TramplelessClient.java
@@ -0,0 +1,7 @@
+package www.wheelershigley.me.trampleless.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TramplelessClient implements ClientModInitializer {
+    @Override public void onInitializeClient() {}
+}
