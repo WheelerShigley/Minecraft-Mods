367c45a814b8990e331344028bf47c0c04631a79
Wheeler-Shigley <115506442+WheelerShigley@users.noreply.github.com>
2025-06-30 13:54:33 -0500

    LilGuy 1.21.7

---
+++ FabricMods/LilGuy/1.21.2-1.21.7/src/main/java/me/wheelershigley/lil_guy/client/LilGuyClient.java
@@ -0,0 +1,7 @@
+package me.wheelershigley.lil_guy.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LilGuyClient implements ClientModInitializer {
+    @Override public void onInitializeClient() {}
+}
