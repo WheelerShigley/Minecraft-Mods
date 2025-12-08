98df78e2a3701fda6111cc835ffa5137feaaac33
WheelerShigley <115506442+WheelerShigley@users.noreply.github.com>
2025-12-08 10:47:56 -0600

    Lil Guy 1.21.9 - 1.21.10

---
+++ FabricMods/LilGuy/1.21.9 - 1.21.10/src/main/java/me/wheelershigley/www/lil_guy/Lil_guy.java
@@ -0,0 +1,12 @@
+package me.wheelershigley.www.lil_guy;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Lil_guy implements ModInitializer {
+    public static String MOD_ID = "lil_guy";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    @Override public void onInitialize() {}
+}
---
+++ FabricMods/LilGuy/1.21.9 - 1.21.10/src/main/java/me/wheelershigley/www/lil_guy/mixins/PassiveEntityMixin.java
@@ -0,0 +1,70 @@
+package me.wheelershigley.www.lil_guy.mixins;
+
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.data.TrackedData;
+import net.minecraft.entity.mob.PathAwareEntity;
+import net.minecraft.entity.passive.PassiveEntity;
+import net.minecraft.particle.ParticleTypes;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(PassiveEntity.class)
+public class PassiveEntityMixin extends PathAwareEntity {
+    protected PassiveEntityMixin(EntityType<? extends PathAwareEntity> entityType, World world) {
+        super(entityType, world);
+    }
+
+    @Shadow public int getBreedingAge() { return 0; }
+    @Shadow public void setBreedingAge(int age) {}
+    @Shadow protected int happyTicksRemaining;
+
+    @Shadow @Final private static TrackedData<Boolean> CHILD;
+
+    /**
+     * @author Wheeler-Shigley
+     * @reason Named baby animals will not have their age changed.
+     */
+    @Inject(
+        method = "tickMovement",
+        at = @At("HEAD"),
+        cancellable = true
+    )
+    public void tickMovement(CallbackInfo ci) {
+        super.tickMovement();
+        if( this.getEntityWorld().isClient() ) {
+            if(this.happyTicksRemaining > 0) {
+                if(this.happyTicksRemaining%4 == 0) {
+                    this.getEntityWorld().addParticleClient(
+                        ParticleTypes.HAPPY_VILLAGER,
+                        this.getParticleX( (double)1.0F ),
+                        this.getRandomBodyY() + (double)0.5F,
+                        this.getParticleZ( (double)1.0F ),
+                        (double)0.0F,
+                        (double)0.0F,
+                        (double)0.0F
+                    );
+                }
+
+                --this.happyTicksRemaining;
+            }
+        } else if( this.isAlive() ) {
+            int breeding_age = this.getBreedingAge();
+            //Babies
+            if(breeding_age < 0 && !this.hasCustomName() ) {
+                ++breeding_age;
+                this.setBreedingAge(breeding_age);
+            }
+            //Adults
+            if(0 < breeding_age) {
+                --breeding_age;
+                this.setBreedingAge(breeding_age);
+            }
+        }
+        ci.cancel();
+    }
+}
