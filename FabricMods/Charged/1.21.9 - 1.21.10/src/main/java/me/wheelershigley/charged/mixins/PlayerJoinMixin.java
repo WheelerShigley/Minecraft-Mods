16b150fff5e3ef79fbff6c68218dec62e59ca4fb
WheelerShigley <115506442+WheelerShigley@users.noreply.github.com>
2025-12-10 12:34:51 -0600

    Charged 1.21.9-1.21.10

---
+++ FabricMods/Charged/1.21.9 - 1.21.10/src/main/java/me/wheelershigley/charged/mixins/PlayerJoinMixin.java
@@ -0,0 +1,35 @@
+package me.wheelershigley.charged.mixins;
+
+import me.wheelershigley.charged.gamerules.WashingGameRulePayload;
+import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
+import net.minecraft.network.ClientConnection;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.PlayerManager;
+import net.minecraft.server.network.ConnectedClientData;
+import net.minecraft.server.network.ServerPlayerEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import static me.wheelershigley.charged.gamerules.GameRuleRegistrar.ENABLE_PLAYER_HEAD_TEXTURE_WASHING;
+
+@Mixin(PlayerManager.class)
+public class PlayerJoinMixin {
+    @Inject(
+        method = "onPlayerConnect",
+        at = @At("TAIL")
+    )
+    public void onPlayerConnect(
+        ClientConnection connection,
+        ServerPlayerEntity player,
+        ConnectedClientData clientData,
+        CallbackInfo ci
+    ) {
+        MinecraftServer server = player.getEntityWorld().getServer();
+        WashingGameRulePayload payload = new WashingGameRulePayload(
+            server.getGameRules().getBoolean(ENABLE_PLAYER_HEAD_TEXTURE_WASHING)
+        );
+        ServerPlayNetworking.send(player, payload);
+    }
+}
---
+++ FabricMods/Charged/1.21.9 - 1.21.10/src/main/java/me/wheelershigley/charged/mixins/PlayerMixin.java
@@ -0,0 +1,78 @@
+package me.wheelershigley.charged.mixins;
+
+import com.mojang.authlib.GameProfile;
+import net.minecraft.component.DataComponentTypes;
+import net.minecraft.component.type.ProfileComponent;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.ItemEntity;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.mob.CreeperEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import static me.wheelershigley.charged.gamerules.GameRuleRegistrar.ENABLE_PLAYER_HEAD_DROP;
+import static me.wheelershigley.charged.gamerules.GameRuleRegistrar.ENABLE_PLAYER_HEAD_DROP_TEXTURES;
+
+@Mixin(ServerPlayerEntity.class)
+public abstract class PlayerMixin extends PlayerEntity {
+    public PlayerMixin(World world, GameProfile profile) {
+        super(world, profile);
+    }
+
+    @Shadow public abstract ItemEntity dropItem(ItemStack stack, boolean throwRandomly, boolean retainOwnership);
+    @Shadow public abstract boolean damage(ServerWorld world, DamageSource source, float amount);
+    @Shadow public abstract ServerWorld getEntityWorld();
+
+    @Inject(
+        method = "onDeath",
+        at = @At("HEAD")
+    )
+    public void onDeath(DamageSource damageSource, CallbackInfo ci) {
+        if(this.getEntityWorld() == null) {
+            return;
+        }
+        boolean doPlayerHeadDrops = this.getEntityWorld().getGameRules().get(ENABLE_PLAYER_HEAD_DROP).get();
+        boolean doPlayerHeadTextureDrops = this.getEntityWorld().getGameRules().get(ENABLE_PLAYER_HEAD_DROP_TEXTURES).get();
+        if(!doPlayerHeadDrops) {
+            return;
+        }
+
+        boolean isKillerAChargedCreeper; {
+            Entity killer = damageSource.getAttacker();
+            isKillerAChargedCreeper =
+                killer instanceof CreeperEntity
+                && ( (CreeperEntity)killer ).isCharged()
+            ;
+        }
+        if(!isKillerAChargedCreeper) {
+            return;
+        }
+
+        ItemStack head = Items.PLAYER_HEAD.getDefaultStack();
+        if(doPlayerHeadTextureDrops) {
+            head.set(
+                DataComponentTypes.PROFILE,
+                ProfileComponent.ofStatic(
+                    this.getGameProfile()
+                )
+            );
+        }
+
+//      head.set(
+//          DataComponentTypes.LORE,
+//          LoreComponent.DEFAULT.with( this.getName() )
+//      );
+
+        this.dropItem(head, true, true);
+    }
+}
