690edecfaad713a55deacfa43aeea1c02cb80cf0
WheelerShigley <115506442+WheelerShigley@users.noreply.github.com>
2025-12-10 10:30:37 -0600

    Charged 1.21.8 (and 1.21.5)

---
+++ FabricMods/Charged/1.21.5 - 1.21.8/src/client/java/www/wheelershigley/me/charged/ReceiverRegistrar.java
@@ -0,0 +1,22 @@
+package www.wheelershigley.me.charged;
+
+import me.wheelershigley.charged.Charged;
+import me.wheelershigley.charged.gamerules.WashingGameRulePayload;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
+import net.minecraft.client.MinecraftClient;
+
+public class ReceiverRegistrar {
+    public static void registerReceiver() {
+        ClientPlayNetworking.registerGlobalReceiver(
+            WashingGameRulePayload.identifier,
+            (payload, context) -> {
+                context.client().execute(
+                    () -> {
+                        Charged.LOGGER.info(String.valueOf(payload.value()));
+                        ChargedClient.isWashingEnabled = payload.value();
+                    }
+                );
+            }
+        );
+    }
+}
