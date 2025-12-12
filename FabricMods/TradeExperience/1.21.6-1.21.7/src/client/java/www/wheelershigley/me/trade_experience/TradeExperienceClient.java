50d2ff037ba85a18f26c534d75df4bf5168c8e11
WheelerShigley <115506442+WheelerShigley@users.noreply.github.com>
2025-12-12 10:53:23 -0600

    TradeExperience 1.21.8
    + 1.21.5, 1.21.8

--- FabricMods/TradeExperience/1.21.6-1.21.7/src/client/java/www/wheelershigley/me/trade_experience/TradeExperienceClient.java
+++
@@ -1,7 +0,0 @@
-package www.wheelershigley.me.trade_experience;
-
-import net.fabricmc.api.ClientModInitializer;
-
-public class TradeExperienceClient implements ClientModInitializer {
-    @Override public void onInitializeClient() {}
-}
