1f9b942e71a54783534b6380882fdbebf9ef2efc
WheelerShigley <115506442+WheelerShigley@users.noreply.github.com>
2026-08-03 17:09:54 -0500

    SilkTouch+ 1.21.11

---
+++ FabricMods/SilkTouchPlus/1.21.11/src/main/java/me/wheelershigley/www/silktouchplus/mixins/SpawnerBlockMixin.java
@@ -0,0 +1,58 @@
+package me.wheelershigley.www.silktouchplus.mixins;
+
+import me.wheelershigley.www.silktouchplus.helpers.EnchantmentsHelper;
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.BaseEntityBlock;
+import net.minecraft.world.level.block.SpawnerBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import static me.wheelershigley.www.silktouchplus.registrations.GameRuleRegistrator.SILKTOUCH_SPAWNER;
+
+@Mixin(SpawnerBlock.class)
+public abstract class SpawnerBlockMixin extends BaseEntityBlock {
+    protected SpawnerBlockMixin(Properties properties) {
+        super(properties);
+    }
+
+    /**
+     * @author wheelershigley
+     * @reason Since spawners drop with SilkTouch, it should not yield Exp when dropped.
+     */
+    @Inject(
+        method = "spawnAfterBreak",
+        at = @At("HEAD"),
+        cancellable = true
+    )
+    protected void spawnAfterBreak(
+        BlockState blockState,
+        ServerLevel serverLevel, BlockPos blockPos,
+        ItemStack tool, boolean bl,
+        CallbackInfo ci
+    ) {
+        super.spawnAfterBreak(blockState, serverLevel, blockPos, tool, bl);
+
+        /* Exp. should not drop if the spawner is silk-touched. */ {
+            if(
+                serverLevel.getGameRules().get(SILKTOUCH_SPAWNER)
+                && EnchantmentsHelper.hasSilkTouch(serverLevel, tool)
+                && tool.is(ItemTags.PICKAXES)
+            ) {
+                bl = false;
+            }
+        }
+        /* Vanilla Implementation */ {
+            if(bl) {
+                int i = 15 + serverLevel.random.nextInt(15) + serverLevel.random.nextInt(15);
+                this.popExperience(serverLevel, blockPos, i);
+            }
+        }
+        ci.cancel();
+    }
+}
---
+++ FabricMods/SilkTouchPlus/1.21.11/src/main/java/me/wheelershigley/www/silktouchplus/registrations/itemDropRegistrations.java
@@ -0,0 +1,92 @@
+package me.wheelershigley.www.silktouchplus.registrations;
+
+import me.wheelershigley.www.silktouchplus.helpers.EnchantmentsHelper;
+import net.fabricmc.fabric.api.event.player.PlayerBlockBreakEvents;
+import net.fabricmc.fabric.api.loot.v3.LootTableEvents;
+import net.minecraft.core.component.DataComponents;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.enchantment.Enchantments;
+import net.minecraft.world.item.enchantment.ItemEnchantments;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.CakeBlock;
+import net.minecraft.world.level.gamerules.GameRules;
+import net.minecraft.world.level.storage.loot.parameters.LootContextParams;
+
+import static me.wheelershigley.www.silktouchplus.registrations.GameRuleRegistrator.*;
+
+public class itemDropRegistrations {
+    public static void registerDirtDropChanges() {
+        LootTableEvents.MODIFY_DROPS.register(
+            (lootTable, context, drops) -> {
+                GameRules gameRules = context.getLevel().getGameRules();
+                ItemStack tool = context.getOptionalParameter(LootContextParams.TOOL);
+                if( tool == null || tool.isEmpty() ) {
+                    return;
+                }
+                boolean wasWithoutSilkTouched = !EnchantmentsHelper.hasSilkTouch(
+                    context.getLevel(),
+                    tool
+                );
+                if(wasWithoutSilkTouched) {
+                    return;
+                }
+
+                if(
+                    (
+                        lootTable.is( Blocks.FARMLAND.getLootTable().orElseThrow() )
+                        && gameRules.get(SILKTOUCH_FARMLAND)
+                    ) || (
+                        lootTable.is( Blocks.DIRT_PATH.getLootTable().orElseThrow() )
+                        && gameRules.get(SILKTOUCH_DIRT_PATH)
+                    )
+                ) {
+                    drops.clear();
+                }
+            }
+        );
+    }
+
+    public static void registerCakeDrop() {
+        PlayerBlockBreakEvents.AFTER.register(
+            (world, player, pos, state, entity) -> {
+                if( !state.getBlock().equals(Blocks.CAKE) ) {
+                    return;
+                }
+
+                boolean shouldCakeBeSilkTouchable = false; {
+                    if(world instanceof ServerLevel serverLevel) {
+                        shouldCakeBeSilkTouchable = serverLevel.getGameRules().get(SILKTOUCH_CAKE);
+                    }
+                }
+                if(!shouldCakeBeSilkTouchable) {
+                    return;
+                }
+
+                boolean toolUsedHasSilkTouch = false; {
+                    ItemStack tool = player.getMainHandItem();
+                    if( !tool.getItem().equals(Items.AIR) ) {
+                        ItemEnchantments enchantments = tool.getOrDefault(DataComponents.ENCHANTMENTS, ItemEnchantments.EMPTY);
+                        if( !enchantments.isEmpty() ) {
+                            toolUsedHasSilkTouch = EnchantmentsHelper.includesEnchantment(
+                                enchantments.entrySet(),
+                                Enchantments.SILK_TOUCH
+                            );
+                        }
+                    }
+                }
+                if(!toolUsedHasSilkTouch) {
+                    return;
+                }
+
+                int bites = state.getValue(CakeBlock.BITES);
+                if(bites == 0) {
+                    CakeBlock.popResource( world, pos, new ItemStack(Items.CAKE) );
+                }
+                return;
+            }
+        );
+    }
+}
