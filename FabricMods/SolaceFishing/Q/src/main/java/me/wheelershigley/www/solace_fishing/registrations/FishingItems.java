8378c112637d2548866d51404df8cf360fd532df
WheelerShigley <115506442+WheelerShigley@users.noreply.github.com>
2026-08-15 15:40:40 -0500

    SolaceFishing Client-Only Description Removal

--- FabricMods/SolaceFishing/Q/src/main/java/me/wheelershigley/www/solace_fishing/registrations/FishingItems.java
+++ FabricMods/SolaceFishing/Q/src/main/java/me/wheelershigley/www/solace_fishing/registrations/FishingItems.java
@@ -69,16 +69,9 @@ public class FishingItems extends PolymerItemsRegister {
         );
 
 
-        final ItemLore rubberDuckLure = new ItemLore(
-            List.of(
-                Component
-                    .translatable("item.solace_fishing.rubber_duck_bobber.description")
-                    .withColor(TextColor.GRAY)
-            )
-        );
         RUBBER_DUCK_BOBBER = register(
             "rubber_duck_bobber",
-            Bobber.DEFAULT_PROPERTIES.component(DataComponents.LORE, rubberDuckLure),
+            Bobber.DEFAULT_PROPERTIES,
             Bobber::new
         );
     }
