c94af220c423860bcfda857324e32bc67bfdecdb
WheelerShigley <115506442+WheelerShigley@users.noreply.github.com>
2026-08-20 15:56:23 -0500

    Window Initialization

---
+++ FabricMods/Window/src/main/java/me/wheelershigley/www/window/Window.java
@@ -0,0 +1,9 @@
+package me.wheelershigley.www.window;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Window implements ModInitializer {
+
+    @Override
+    public void onInitialize() {}
+}
